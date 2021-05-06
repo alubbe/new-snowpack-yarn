78734fe556a815d813459e1805fed2f48b89d7a1
Andreas Lubbe <andreas_lubbe@external.mckinsey.com>
2021-05-06 15:27:31 +0200

    Use yarn 2

--- snowpack.config.js
+++ snowpack.config.js
@@ -7,13 +7,7 @@ module.exports = {
   plugins: [
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
-    [
-      '@snowpack/plugin-typescript',
-      {
-        /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
-        ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
-      },
-    ],
+    '@snowpack/plugin-typescript',
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
