27942582b33e7fd3eb6e8c9cc3563d5e6cce5c31
Andreas Lubbe <andreas_lubbe@external.mckinsey.com>
2021-05-17 14:48:17 +0200

    Simplify

--- snowpack.config.js
+++ snowpack.config.js
@@ -9,19 +9,16 @@ module.exports = {
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
   ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
   optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
+    treeshake: true,
+    bundle: true,
+    splitting: true,
+    minify: true,
+    sourcemap: 'external',
+    target: 'es2020',
   },
   packageOptions: {
     polyfillNode: true,
-    optimizie: {
-      treeshake: true,
-    },
     namedExports: [
       '@mid/driver-okta',
       '@mid/react-widget/redirect',
